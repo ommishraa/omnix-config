7134942d135915f0165f289e283a1c7ccacebe92
ommishraa <ommishra.commerce@gmail.com>
2026-03-13 22:05:12 +0530

    fixed whole configuration

--- home/home.nix
+++ home/home.nix
@@ -6,14 +6,14 @@
   xdg.configFile = {
     "cava".source = ./programs/cava;
     "fastfetch".source = ./programs/fastfetch;
-    "kitty".source = ./programs/kitty;
+    #"kitty".source = ./programs/kitty;
     "rofi".source = ./programs/rofi;
     "swaync".source = ./programs/swaync;
     "tmux".source = ./programs/tmux;
     "waybar".source = ./programs/waybar;
     "wlogout".source = ./programs/wlogout;
 
-    "hypr".source = ./hypr;
+    #"hypr".source = ./hypr;
     "matugen".source = ./matugen;
   };
 }
